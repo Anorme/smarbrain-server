c4b53e66d6137b55086970bc05dbacb8bab3812a
Anorme <anormeinkumsah@gmail.com>
2024-12-21 22:59:14 +0100

    cleaned up troubleshooting code

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,6 @@
 
  const handleRegister = (db, bcrypt) => (req, res) => {
 	const {email, name, password} = req.body;
-	console.log('Received registration data:', {email, name, password}) //TS
 
 	if(!email || !name || !password) {
 		return res.status(400).json('incorrect form submission');
@@ -15,7 +14,6 @@
 			.into('login')
 			.returning('email')
 			.then(loginEmail => {
-				console.log('Inserted into login table:', loginEmail ); //TS
 				return trx('users')
 				.returning('*')
 				.insert({
@@ -24,23 +22,17 @@
 					joined: new Date()
 				})
 				.then(user => {
-					console.log('Inserted into users table:', user); //TS
 					res.json(user[0]);
 				})
 				.catch(err => {
-					console.error('Error inserting into user:' ,err); //TS
 					trx.rollback();
 					res.status(400).json('unable to register');
 				});
 			})
 			.then(trx.commit)
-			.catch(err => {
-				console.error('Transaction commit error:', err) //TS
-				trx.rollback
-			})
+			.catch(trx.rollback)
 		})
 		.catch(err => {
-			console.error('Transaction commit error', err); //TS
 			res.status(400).json('unable to register user');
 		});
 }
