5790710741d9434d5578fa3dc67ae9ac5bf4f6f5
Anorme <anormeinkumsah@gmail.com>
2024-12-21 21:56:43 +0100

    Console log pro max

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,8 @@
 
  const handleRegister = (db, bcrypt) => (req, res) => {
 	const {email, name, password} = req.body;
+	console.log('Received registration data:', {email, name, password}) //TS
+
 	if(!email || !name || !password) {
 		return res.status(400).json('incorrect form submission');
 	}
@@ -13,6 +15,7 @@
 			.into('login')
 			.returning('email')
 			.then(loginEmail => {
+				console.log('Inserted into login table:', loginEmail ); //TS
 				return trx('users')
 				.returning('*')
 				.insert({
@@ -21,19 +24,23 @@
 					joined: new Date()
 				})
 				.then(user => {
+					console.log('Inserted into users table:', user); //TS
 					res.json(user[0]);
 				})
 				.catch(err => {
-					console.error(err);
+					console.error('Error inserting into user:' ,err); //TS
 					trx.rollback();
 					res.status(400).json('unable to register');
 				});
 			})
 			.then(trx.commit)
-			.catch(trx.rollback)
+			.catch(err => {
+				console.error('Transaction commit error:', err) //TS
+				trx.rollback
+			})
 		})
 		.catch(err => {
-			console.error(err);
+			console.error('Transaction commit error', err); //TS
 			res.status(400).json('unable to register user');
 		});
 }
